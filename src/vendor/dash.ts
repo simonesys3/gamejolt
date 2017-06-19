4d9b46588b5ef0ed41882d2281772998f57ae877
David DeCarmine <hworld@holo-world.com>
2017-06-19 11:46:53 -0600

    Fix datepicker not showing

--- src/vendor/dash.ts
+++ src/vendor/dash.ts
@@ -5,3 +5,6 @@ import '../bower-lib/angular-bootstrap/src/timepicker/timepicker';
 // Bootstrap templates.
 import '!ng-cache-loader?module=ui.bootstrap.timepicker.tpls&prefix=template/timepicker/!../lib/gj-lib-client/components/timepicker/timepicker.html';
 import '!ng-cache-loader?module=ui.bootstrap.datepicker.tpls&prefix=template/datepicker/!../lib/gj-lib-client/components/datepicker/datepicker.html';
+import '!ng-cache-loader?module=ui.bootstrap.datepicker.tpls&prefix=template/datepicker/!../lib/gj-lib-client/components/datepicker/day.html';
+import '!ng-cache-loader?module=ui.bootstrap.datepicker.tpls&prefix=template/datepicker/!../lib/gj-lib-client/components/datepicker/month.html';
+import '!ng-cache-loader?module=ui.bootstrap.datepicker.tpls&prefix=template/datepicker/!../lib/gj-lib-client/components/datepicker/year.html';
